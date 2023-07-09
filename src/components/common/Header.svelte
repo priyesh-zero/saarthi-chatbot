899bbc6c2a929cce532434344db10b719cb7925b
Priyesh Shrivastava <priyesh.zero@gmail.com>
2023-07-09 06:21:32 +0530

    refactor(configuration): ⚙ Alias Configuration
    Adding configuration for alias in `vite.config.js` to make the compiler resolve the alias and moved
    the header component to sepercommon

---
+++ src/components/common/Header.svelte
@@ -0,0 +1,7 @@
+<header>
+	<h1 class="scale-150 text-2xl text-blue-300 underline">
+		<span class="font-macondo text-gray-500">Aapka</span><span
+			class="font-tangerine text-5xl font-extrabold text-blue-400">Sarthi</span
+		>
+	</h1>
+</header>
--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,7 +1,5 @@
-<header>
-	<h1 class="scale-150 text-2xl text-blue-300 underline">
-		<span class="font-macondo text-gray-500">Aapka</span><span
-			class="font-tangerine text-5xl font-extrabold text-blue-400">Sarthi</span
-		>
-	</h1>
-</header>
+<script lang="ts">
+	import Header from '@/components/common/Header.svelte';
+</script>
+
+<Header />
